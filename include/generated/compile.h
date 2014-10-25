fb98416fcd7203b84929dbc92f1c1d4095d03e91
Thanos Alexopoulos <talexop@gmail.com>
2014-10-25 11:31:02 +0300

    Update9 fauxsound 3.5

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Fri Oct 24 15:40:34 EEST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "talexop-linux"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
